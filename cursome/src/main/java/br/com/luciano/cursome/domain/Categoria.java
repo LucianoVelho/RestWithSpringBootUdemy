5bd30c3de2d3052d6fb46b422469a4d864d7bfc7
Luciano Velho <lucianovelgarcia@gmail.com>
2021-12-01 11:00:51 -0300

    Configuração H2

--- cursome/src/main/java/br/com/luciano/cursome/domain/Categoria.java
+++ cursome/src/main/java/br/com/luciano/cursome/domain/Categoria.java
@@ -6,15 +6,21 @@ package br.com.luciano.cursome.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
  * @author Luciano Velho Garcia
  */
+@Entity
 public class Categoria implements Serializable{
     
     private static final long serialVersionUID = 1L;
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
